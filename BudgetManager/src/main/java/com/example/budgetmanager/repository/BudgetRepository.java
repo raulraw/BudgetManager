1f497c41b31c67bfbe0fbb398f3d4cb67fe9bcd9
Staicu Raul-Valentin <raul_valentin123@yahoo.com>
2024-11-19 18:44:06 +0200

    Delete BudgetManager directory

--- BudgetManager/src/main/java/com/example/budgetmanager/repository/BudgetRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.budgetmanager.repository;
-
-import com.example.budgetmanager.entity.Budget;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BudgetRepository extends JpaRepository<Budget, Long> {
-    // Poți adăuga metode personalizate pentru a căuta bugetul
-}
